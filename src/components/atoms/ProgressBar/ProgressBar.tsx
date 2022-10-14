ad28b76412fbca773f97642dc1626aac35462cb6
Santiago Alvarez Rozo <alvarezrozo@gmail.com>
2022-10-13 22:52:52 -0500

    Added mock data

--- src/components/atoms/ProgressBar/ProgressBar.tsx
+++ src/components/atoms/ProgressBar/ProgressBar.tsx
@@ -3,16 +3,17 @@ import './ProgressBar.scss'
 interface Props {
   totalSteps: number
   currentStep: number
+  className?: string
 }
 
 const ProgressBar = (props:Props) => {
-  const { totalSteps, currentStep } = props
+  const { totalSteps, currentStep, className = '' } = props
 
-  const currentPercentage =  (currentStep / (totalSteps + 1)) * 100
+  const currentPercentage =  ((currentStep + 1) / (totalSteps + 1)) * 100
   const currentPercentageDrawer = currentPercentage > 100 ? 100 : currentPercentage
 
   return (
-    <progress max="100" value={currentPercentageDrawer} className="bv-progress-bar">
+    <progress max="100" value={currentPercentageDrawer} className={`bv-progress-bar ${className}`}>
       {`${currentPercentageDrawer}%`}
     </progress>
   )
