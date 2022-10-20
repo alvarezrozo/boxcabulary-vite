cf08875e6f96a2af33d031db928d7c634fc6ea4c
Santiago Alvarez Rozo <alvarezrozo@gmail.com>
2022-10-19 19:36:06 -0500

    Added languages to types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,7 @@
 export type NAV_ITEM_NAMES = 'home' | 'user'
 export type INPUT_STATES = 'filled' | 'error' | 'success' | undefined
 export type BUTTON_THEME_COLORS = 'blue' | 'green' | 'red' | undefined
+export type LANGUAGES = 'spanish' | 'english'
 
 export interface IWord {
   score: number
